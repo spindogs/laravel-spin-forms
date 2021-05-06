38ccad48b4e58feabd792c78c4cc2e917f804d10
Adam Chapman <achapman@spindogs.com>
2021-05-06 17:21:01 +0100

    [FIX] Label was not obeying attribute designation, so not displaying class / for etc

--- resources/views/components/form-label.blade.php
+++ resources/views/components/form-label.blade.php
@@ -1,6 +1,6 @@
 @if ($label)
     <div class="label_wrap">
-        <label>
+        <label {!! $attributes->merge() !!}>
             {{ $label }}@if ($required)<span class="required">*</span>@endif
         </label>
     </div>
