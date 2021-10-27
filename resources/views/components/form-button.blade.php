e2e0a0dc2a1e158206d6ca0ee4d6faed5704c3c7
Adam Chapman <achapman@spindogs.com>
2021-10-27 20:21:54 +0100

    All attributes to be able to be put on a button

--- resources/views/components/form-button.blade.php
+++ resources/views/components/form-button.blade.php
@@ -1,7 +1,7 @@
 @if ($type == 'submit')
     <div class="submit_wrap">
 @endif
-    <button type="{{ $type }}">
+    <button {{ $attributes->merge(['type' => $type]) }}>
         {!! trim($slot) ?: __('Submit') !!}
     </button>
 @if ($type == 'submit')
